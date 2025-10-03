80b870d183705357bd5b3ad18eb932deedde7ed0
Admin <pthao101023@gmail.com>
2025-10-03 13:10:09 +0700

    Project structure after build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     war
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
 }
 
 group = "com"
@@ -30,11 +31,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:12.10.1.jre11")
+    implementation("com.microsoft.sqlserver:mssql-jdbc:12.10.0.jre11")
+
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+    implementation(kotlin("stdlib-jdk8"))
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
